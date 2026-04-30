073a999e350a120d79c608582fd04fdc132c9965
Thyago Euclides <flooddk@gmail.com>
2026-04-30 12:10:25 -0400

    Implement Resume presentation

---
+++ src/windows/Resume.jsx
@@ -0,0 +1,38 @@
+import { WindowControls } from "#components/index.js";
+import WindowWrapper from "#hoc/WindowWrapper.jsx";
+import { Download } from "lucide-react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url,
+).toString();
+
+const Resume = () => {
+  return (
+    <>
+      <div id="window-header">
+        <WindowControls target="resume" />
+        <h2>Resume.pdf</h2>
+
+        <a
+          href="files/resume.pdf"
+          download
+          className="cursor-pointer"
+          title="Download resume"
+        >
+          <Download className="icon" />
+        </a>
+      </div>
+
+      <Document file="https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf">
+        <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+      </Document>
+    </>
+  );
+};
+
+const ResumeWindow = WindowWrapper(Resume, "resume");
+export default ResumeWindow;
